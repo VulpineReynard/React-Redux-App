4b8031fd34195e4d4fddd12e6dfd25fa159cdd4c
VulpineReynard <milare8@gmail.com>
2019-08-15 18:27:58 -0400

    basic app setup, next is to organize data flow in redux

---
+++ redux-app/src/Components/Fox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Fox = () => {
+  return (
+    <div className="fox-container">
+
+    </div>
+  );
+}
+
+export default Fox;
---
+++ redux-app/src/Components/FoxGeneratorForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FoxGeneratorForm = () => {
+  return (
+    <form className="fox-form">
+      <button>
+        Generate Me A Fox
+      </button>
+    </form>
+  );
+}
+
+export default FoxGeneratorForm;
